b99d79ccd9020a16a8fd3cd55ccfc6701032f2b1
marijedb <marijedb@live.nl>
2022-10-28 22:49:36 +0200

    updated dices

--- index.js
+++ index.js
@@ -2,25 +2,34 @@ const wizard = {
    name: "Wizard",
    avatar: "images/wizard.png",
    health: 60,
-   diceScore: 6,
-   id: "hero"
+   diceScore: [3, 1, 4],
+   id: "hero",
+   diceCount: 3
 }
 
 const monster = {
    name: "Orc",
    avatar: "images/orc.png",
    health: 10,
-   diceScore: 4,
-   id: "monster"
+   diceScore: [2],
+   id: "monster",
+   diceCount: 1
 }
 
 function renderCharacter(data) {
-   document.getElementById(data.id).innerHTML= `
+   const {name, avatar, health, diceScore, id, diceCount} = data;
+   let diceHtml = ``;
+
+   for(let i = 0; i < diceCount; i++){
+      diceHtml += `<div class="dice">${diceScore[i]}</div>`;
+   }
+
+   document.getElementById(id).innerHTML= `
        <div class="character-card">
-           <h4 class="name">${data.name}</h4>
-           <img class="avatar" src="${data.avatar}"/>
-           <p class="health">health: <b> ${data.health} </b></p>
-           <div class="dice-container"><div class="dice"> ${data.diceScore} </div></div>
+           <h4 class="name">${name}</h4>
+           <img class="avatar" src="${avatar}"/>
+           <p class="health">health: <b> ${health} </b></p>
+           <div class="dice-container">${diceHtml}</div>
        </div>
    `
 }
