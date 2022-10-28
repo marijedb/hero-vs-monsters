4780646324e761475c38e373e6f6072671f2517f
marijedb <marijedb@live.nl>
2022-10-28 21:26:41 +0200

    small fix in html file and removed comment js

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
-
 const hero = document.getElementById("hero");
 const monster = document.getElementById("monster");
 
